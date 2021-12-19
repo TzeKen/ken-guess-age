1bbc7a3e9f6fecc68a49a9a3d7d949a239f7d3b5
TzeKen <tantzeken05@gmail.com>
2021-12-19 03:33:55 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    let user_name ="";
+    let age ="";
+
+    async function getAge() {
+        fetch(' https://api.agify.io/?name=' + user_name)
+        .then((response) => response.json())
+        .then((data) => {
+  	       user_name = data.name;
+  	       age = data.age; 
+	    });
+    }
+</script>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="user_name" bind:value={user_name} type="text" />
+    <button type="submit">Check</button>
+</form>
